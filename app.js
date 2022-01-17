7ce06ab82d9d2a764ba4e30565e26b8a5d53befd
AlvaroAlfonso <alvaroandresinho@hotmail.es>
2022-01-17 02:02:05 -0500

    agrego todo lo relacionado a MercadoLiebre

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require ("path");
+
+const publicPath = path.resolve(__dirname,"./public");
+
+app.use(express.static(publicPath));
+
+app.get ("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.post ("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get ("/register", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+app.post ("/register", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+app.get ("/login", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+app.post ("/login", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+app.listen (3001, ()=>{
+    console.log("Corriendo en el serivor 3001")
+})
